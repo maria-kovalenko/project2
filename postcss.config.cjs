01543a7cb7ccb24f104743f7be03882463b31b3f
Maria <m1a2r3.kovalenko@gmail.com>
2022-10-12 22:24:15 +0300

    init: start project2

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+// import cssnano from 'cssnano';
+
+module.exports = {
+  plugins: {
+    // autoprefixer: {},
+    // 'postcss-uncss': {
+    //     html: ['pages/**/*.html'],
+    // },
+    // 'cssnano': cssnano({
+    //   preset: 'default',
+    // }),
+  },
+}
