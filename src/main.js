f9e509de14024360b4cffc4b8de010e2e921c9ad
Maria <m1a2r3.kovalenko@gmail.com>
2022-10-13 21:24:36 +0300

    fix: fix slider input range

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "@styles/main.scss";
 
-// -------------------timer-------------------------------
+// timer
 let time = 180;
 let intr;
 function tick() {
@@ -11,7 +11,6 @@ function tick() {
     clearInterval(intr);
   }
   secs = secs >= 10 ? secs : "0" + secs;
-  mins = mins >= 10 ? mins : "0" + mins;
 
   let min = document.getElementById("min");
   min.innerHTML = mins;
@@ -25,3 +24,55 @@ function start_timer() {
 }
 
 start_timer();
+
+// input range
+var sheet = document.createElement("style"),
+  $rangeInput = $(".range input"),
+  prefs = ["webkit-slider-runnable-track", "moz-range-track", "ms-track"];
+
+document.body.appendChild(sheet);
+
+var getTrackStyle = function (el) {
+  var curVal = el.value,
+    val = (curVal - 1) * 32.666666667,
+    style = "";
+
+  // Set active label
+  $(".range-labels li").removeClass("active selected");
+
+  var curLabel = $(".range-labels").find("li:nth-child(" + curVal + ")");
+
+  curLabel.addClass("active selected");
+  curLabel.prevAll().addClass("selected");
+
+  // Change background gradient
+  for (var i = 0; i < prefs.length; i++) {
+    style +=
+      ".range {background: linear-gradient(to right, #C70122 0%, #C70122 " +
+      val +
+      "%, #fff " +
+      val +
+      "%, #fff 100%)}";
+    style +=
+      ".range input::-" +
+      prefs[i] +
+      "{background: linear-gradient(to right, #C70122 0%, #C70122 " +
+      val +
+      "%, #f3f3f3 " +
+      val +
+      "%, #f3f3f3 100%)}";
+  }
+
+  return style;
+};
+
+$rangeInput.on("input", function () {
+  sheet.textContent = getTrackStyle(this);
+});
+
+// Change input value on label click
+$(".range-labels li").on("click", function () {
+  var index = $(this).index();
+
+  $rangeInput.val(index + 1).trigger("input");
+});
