9af54bcd8f5fbcc4d27fea03a4cab53e7c83038e
Maria <m1a2r3.kovalenko@gmail.com>
2022-10-13 12:44:24 +0300

    feat: add timer

--- src/main.js
+++ src/main.js
@@ -50,3 +50,29 @@ $(".range-labels li").on("click", function () {
 
   $rangeInput.val(index + 1).trigger("input");
 });
+
+// -------------------timer-------------------------------
+let time = 180;
+let intr;
+function tick() {
+  time = time - 1;
+  let mins = Math.floor(time / 60);
+  let secs = time - mins * 60;
+  if (mins == 0 && secs == 0) {
+    clearInterval(intr);
+  }
+  secs = secs >= 10 ? secs : "0" + secs;
+  mins = mins >= 10 ? mins : "0" + mins;
+
+  let min = document.getElementById("min");
+  min.innerHTML = mins;
+
+  let sec = document.getElementById("sec");
+  sec.innerHTML = secs;
+}
+
+function start_timer() {
+  intr = setInterval(tick, 1000);
+}
+
+start_timer();
