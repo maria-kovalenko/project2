90da339b5bab636c0a5f77cc8d3a8f704fd18417
Сергей <sergiodrovski@gmail.com>
2022-10-14 14:13:26 +0300

    refactor: new structur

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,3 @@
-// import "@styles/main.scss";
 
 //-------------------Checkbox Border-------------------------------
 
@@ -24,76 +23,76 @@ for (let elem of allCheckbox) {
 
 
 //-------------------Slider-------------------------------
-// const allCard = document.querySelectorAll('.card')
-// const buttonNext = document.querySelector('#button_next')
-// const arrowLeft = document.querySelector('.header__arrow-left')
-// const arrowRight = document.querySelector('.header__arrow-right')
-// const elemPageNum = document.querySelector('.current-number')
-// const elemScroll = document.querySelector('.header__scrollbal')
+const allCard = document.querySelectorAll('.card')
+const buttonNext = document.querySelector('#button_next')
+const arrowLeft = document.querySelector('.header__arrow-left')
+const arrowRight = document.querySelector('.header__arrow-right')
+const elemPageNum = document.querySelector('.current-number')
+const elemScroll = document.querySelector('.header__scrollbal')
 
-// class SlidCart {
-//   constructor() {
-//     this.prev = null;
-//     this.current = 1;
-//     this.slideLength = allCard.length;
-//   }
+class SlidCart {
+  constructor() {
+    this.prev = null;
+    this.current = 1;
+    this.slideLength = allCard.length;
+  }
 
-//   handleEvent(event) {
-//     if(event.currentTarget.matches('#button_next')) { return this.nextSlide()}
-//     if(event.currentTarget.matches('.header__arrow-left')) { return this.prevSlide()}
-//     if(event.currentTarget.matches('.header__arrow-right')) { return this.nextSlide()}
-//   }
+  handleEvent(event) {
+    if(event.currentTarget.matches('#button_next')) { return this.nextSlide()}
+    if(event.currentTarget.matches('.header__arrow-left')) { return this.prevSlide()}
+    if(event.currentTarget.matches('.header__arrow-right')) { return this.nextSlide()}
+  }
 
-//   nextSlide() {
-//     if(this.current === this.slideLength) return;
-//     this.prev = this.current
-//     this.current++
-//     this.showSlide()
-//   }
+  nextSlide() {
+    if(this.current === this.slideLength) return;
+    this.prev = this.current
+    this.current++
+    this.showSlide()
+  }
 
-//   prevSlide() {
-//     if(this.current === 1) return;
-//     this.prev = this.current
-//     this.current--
-//     this.showSlide()
-//   }
+  prevSlide() {
+    if(this.current === 1) return;
+    this.prev = this.current
+    this.current--
+    this.showSlide()
+  }
 
-//   showSlide() {
-//     let showSlide = allCard[this.current - 1]
-//     showSlide.classList.add("show")
-//     if(this.prev) {
-//       let hiddenSlide = allCard[this.prev - 1]
-//       hiddenSlide.classList.remove("show")
-//     }
-//     this.setTextButton()
-//     this.scrollBar()
-//   }
+  showSlide() {
+    let showSlide = allCard[this.current - 1]
+    showSlide.classList.add("show")
+    if(this.prev) {
+      let hiddenSlide = allCard[this.prev - 1]
+      hiddenSlide.classList.remove("show")
+    }
+    this.setTextButton()
+    this.scrollBar()
+  }
 
-//   setTextButton() {
-//     if (this.current === 1 ){
-//       buttonNext.innerHTML = "Начать";
-//       return
-//     }
-//     if (this.current > 1 ){
-//       buttonNext.innerHTML = "Далее";
-//     }
-//     if(this.current === this.slideLength){
-//       buttonNext.innerHTML = "Начать зарабатывать";
-//     }
-//   }
+  setTextButton() {
+    if (this.current === 1 ){
+      buttonNext.innerHTML = "Начать";
+      return
+    }
+    if (this.current > 1 ){
+      buttonNext.innerHTML = "Далее";
+    }
+    if(this.current === this.slideLength){
+      buttonNext.innerHTML = "Начать зарабатывать";
+    }
+  }
 
-//   scrollBar(){
-//     elemPageNum.innerHTML = `${this.current}`;
-//     let widthLine =  Math.round(elemScroll.offsetWidth / this.slideLength)
-//     elemScroll.children[0].style.width = `${widthLine}px`
-//     elemScroll.children[0].style.marginLeft = `${widthLine * (this.current - 1)}px`
-//   }
-// }
-// let slidCart = new SlidCart();
-// slidCart.showSlide();
-// buttonNext.addEventListener('pointerdown', slidCart);
-// arrowLeft.addEventListener('pointerdown', slidCart);
-// arrowRight.addEventListener('pointerdown', slidCart);
+  scrollBar(){
+    elemPageNum.innerHTML = `${this.current}`;
+    let widthLine =  Math.round(elemScroll.offsetWidth / this.slideLength)
+    elemScroll.children[0].style.width = `${widthLine}px`
+    elemScroll.children[0].style.marginLeft = `${widthLine * (this.current - 1)}px`
+  }
+}
+let slidCart = new SlidCart();
+slidCart.showSlide();
+buttonNext.addEventListener('pointerdown', slidCart);
+arrowLeft.addEventListener('pointerdown', slidCart);
+arrowRight.addEventListener('pointerdown', slidCart);
 
 // -------------------timer-------------------------------
 
